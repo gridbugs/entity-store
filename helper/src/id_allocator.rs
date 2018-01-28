9e27b86ad24cfb83c529617a5cdc785584c68906
Stephen Sherratt <sfsherratt@gmail.com>
2018-01-28 19:14:41 +0000

    Add helper crate

---
+++ helper/src/id_allocator.rs
@@ -0,0 +1,34 @@
+use num::Integer;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct IdAllocator<T: Integer + Copy>  {
+    next: T,
+    free_list: Vec<T>,
+}
+
+impl<T: Integer + Copy> IdAllocator<T> {
+    pub fn new() -> Self {
+        Self {
+            next: T::zero(),
+            free_list: Vec::new(),
+        }
+    }
+
+    pub fn allocate(&mut self) -> T {
+        if let Some(id) = self.free_list.pop() {
+            id
+        } else {
+            let id = self.next;
+            self.next = self.next + T::one();
+            id
+        }
+    }
+
+    pub fn peek(&self) -> T {
+        self.next
+    }
+
+    pub fn free(&mut self, id: T) {
+        self.free_list.push(id);
+    }
+}
