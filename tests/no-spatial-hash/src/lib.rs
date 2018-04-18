13ba4d70689bbcb0b9a72517bf0a522ba1dcf1db
Stephen Sherratt <stephen@sherra.tt>
2018-04-17 23:09:04 -0400

    Basic test of no-spatial-hash

--- tests/no-spatial-hash/src/lib.rs
+++ tests/no-spatial-hash/src/lib.rs
@@ -7,3 +7,18 @@ extern crate serde_derive;
 pub mod entity_store {
     include_entity_store!("entity_store.rs");
 }
+
+#[cfg(test)]
+mod tests {
+
+    use entity_store::*;
+
+    #[test]
+    fn basic() {
+        let (mut store, wit) = EntityStore::new();
+        let id_a = store.allocate_entity_id(&wit);
+        store.insert_foo(id_a, 42);
+
+        assert_eq!(store.get_foo(id_a), Some(&42));
+    }
+}
